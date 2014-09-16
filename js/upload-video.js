ec96e3fd990e790128b38d7f50c6dad50804a528
pangyuanxing <308106832@qq.com>
2014-09-16 15:38:35 +0800

    qq视频上传

--- js/upload-video.js
+++ js/upload-video.js
@@ -8,6 +8,85 @@
         return uri;
     };
 
+    //这一段是腾讯视频给的把url转成vid
+    function getVidFromUrl(url) {
+        url = url || window.location.toString();
+        //先从url中分析出vid参数，例如×××××.html?vid=××××××
+        var vid = getUrlParam("vid", url),
+            r;
+        if (!vid) { // 使用新规则生成的专辑单视频页面
+            if (r = url.match(/\/\w{15}\/(\w+)\.html/)) {
+                vid = r[1];
+            }
+        }
+        // 单视频播放页
+        if (!vid) {
+            if (r = url.match(/\/page\/\w{1}\/\w{1}\/\w{1}\/(\w+)\.html/)) {
+                vid = r[1];
+            } else if (r = url.match(/\/(page|play)\/+(\w{11})\.html/)) {
+                vid = r[2];
+            }
+        }
+        // 播客专辑播放页
+        if (!vid) {
+            if (r = url.match(/\/boke\/gplay\/\w+_\w+_(\w+)\.html/)) {
+                vid = r[1];
+            }
+        }
+        return encodeURIComponent(vid);
+    }
+
+    function getUrlParam(p, u) {
+        var u = u || document.location.toString();
+        var pa = p + "=";
+        var f = u.indexOf(pa);
+        if (f != -1) {
+            var f2 = u.indexOf("&", f);
+            var f2p = u.indexOf("?", f);
+            if (f2p != -1 && (f2 == -1 || f2 > f2p))
+                f2 = f2p;
+            f2p = u.indexOf("#", f);
+            if (f2p != -1 && (f2 == -1 || f2 > f2p))
+                f2 = f2p;
+            if (f2 == -1)
+                return u.substring(f + pa.length);
+            else
+                return u.substring(f + pa.length, f2);
+        }
+        return "";
+    }
+
+    //关于腾讯视频 有问题请找popotang
+    function getUrlVid( link, callback){
+        var vid = "", r, flashvars = "", return_url;
+        //with vid in url query str
+        if( r = link.match( new RegExp("(^|&|\\\\?)vid=([^&]*)(&|$|#)") )) {
+            vid = encodeURIComponent(r[2]);
+            callback( vid );
+        }
+        //with cid in url
+        //如果有连续剧的视频。需要jsonp去腾讯视频后台用cid换取一段json，然后取连续剧的第一集开始播放
+        else if( r = link.match( new RegExp("(http://)?v\\.qq\\.com/cover[^/]*/\\w+/([^/]*)\\.html") ) )
+        {
+            var cid = encodeURIComponent(r[2]),
+                path = 'http://sns.video.qq.com/fcgi-bin/dlib/dataout_ex?auto_id=137&cid=' + cid + '&otype=json';
+
+            $.getScript(path, function(){
+                vid = QZOutputJson['videos'][0]['vid'];
+                callback( vid );
+            });
+        }
+        //with vid in url
+        //这里用腾讯视频给的逻辑来做
+        else{
+            vid = getVidFromUrl(link);
+            callback( vid );
+
+        }
+    }
+
+    var vid;
+
     $(document).on('click', '.get-video-btn', function(){
         var $this = $(this), 
             input = $this.closest('.video-item').find('input'),
@@ -19,35 +98,59 @@
                 alert("该视频已转换过，请不要重复执行!");
             } else {
                 $('.loading-box').show(0);
-                insertBtn.attr('data-video-url' , url);
-                var videoUrlParser = $.ajax({
-                    url : 'http://baoxiaoyike.sinaapp.com/VideoUrlParser/server.php',
-                    dataType : 'jsonp',
-                    data : {
-                        url : url
-                    },
-                    success : function(res){
-                        if( res.status ) {
-                            var poster = res.data.img;
-                            var title = res.data.title;
-                            var rex = /(\w+)\/\//g;
-                            poster = poster.replace(rex, '$1/');
-                            if( poster ) {
+                var vqq = /v\.qq\.com/,
+                    isQq = vqq.test( url ),
+                    title,
+                    poster;
+                if( isQq ) {
+                    getUrlVid(url, function($vid){
+                        vid = $vid;
+                        $.ajax({
+                            url : 'http://vv.video.qq.com/getinfo?vids='+ vid + '&otype=json',
+                            dataType : 'jsonp',
+                            success : function( data ){
+                                title = data.vl.vi[0].ti;
+                                poster = 'http://shp.qpic.cn/qqvideo_ori/0/' + vid + '_496_280/0';
+                                url = 'http://v.qq.com/iframe/player.html?vid=' + vid;
+                                insertBtn.attr('data-video-url' , url);
                                 insertBtn.attr('data-video-poster' , poster);
                                 $('#title').val( title );
                             }
-                            alert("转换成功");
-                        } else {
-                           alert('转换失败,请重新试一下!'); 
+                        });
+                    });
+                } else {
+                    insertBtn.attr('data-video-url' , url);
+                    var videoUrlParser = $.ajax({
+                        url : 'http://baoxiaoyike.sinaapp.com/VideoUrlParser/server.php',
+                        dataType : 'jsonp',
+                        data : {
+                            url : url
+                        },
+                        success : function(res){
+                            if( res.status ) {
+                                var poster = res.data.img;
+                                var title = res.data.title;
+                                var rex = /(\w+)\/\//g;
+                                poster = poster.replace(rex, '$1/');
+                                if( poster ) {
+                                    insertBtn.attr('data-video-poster' , poster);
+                                }
+                                if( title ) {
+                                    $('#title').val( title );
+                                }
+                                alert("转换成功");
+                            } else {
+                                alert('转换失败,请重新试一下!');
+                            }
+                        },
+                        complete : function(){
+                            $('.loading-box').hide(0);
+                        },
+                        error : function() {
+                            alert('转换失败,请重新试一下!');
                         }
-                    },
-                    complete : function(){
-                        $('.loading-box').hide(0);
-                    },
-                    error : function() {
-                        alert('转换失败,请重新试一下!');
-                    }
-                });
+                    });
+                }
 
             }
             
